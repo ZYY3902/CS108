d047d46ce4a204a10b5786149f686e6368bb66f2
Zining Ye <ziningye@bu.edu>
2021-11-03 03:18:26 -0400

    Refined model design to create Person, Quote, and Image models.

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,9 +1,12 @@
 # quotes/urls.py
 
 from django.urls import path
-from .views import HomePageView # our view class definition 
+from .views import HomePageView, PersonPageView, QuotePageView, RandomQuotePageView # our view class definition 
 
 urlpatterns = [
     # map the URL to the view
-    path('', HomePageView.as_view(), name='home_page'),
+    path('all', HomePageView.as_view(), name='all_quotes'), 
+    path('quote/<int:pk>', QuotePageView.as_view(), name='quote'),
+    path('random', RandomQuotePageView.as_view(), name='random'),  # show one quote at random
+    path('person/<int:pk>', PersonPageView.as_view(), name="person"), ## NEW URL TO SHOW PERSON PAGE
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Quote
+from django.views.generic import ListView, DetailView
+from .models import Quote, Person
+import random
 
 # Create your views here.
 
@@ -10,3 +11,31 @@ class HomePageView(ListView):
     model = Quote # retrieve Quote objects from the database
     template_name = 'quotes/home.html'
     context_object_name = 'quotes' # how to find the data in the template file
+
+class QuotePageView(DetailView):
+    '''Display a single quote object'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+class RandomQuotePageView(DetailView):
+    '''Display a single quote object'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+    def get_object(self):
+        '''Return one Quote object chosen at random.'''
+
+        #obtain all quotes using the object manager
+        quotes = Quote.objects.all()
+        q = random.choice(quotes)
+        return q
+
+
+class PersonPageView(DetailView):
+    '''Display a single Person object'''
+
+    model = Person
+    template_name = 'quotes/person.html'
+    context_object_name = 'person'
